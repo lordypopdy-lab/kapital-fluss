bc71fd2b89b19b5e66d3eabfdc4906654b34e558
James Isaac <jamesisaac@Jamess-MacBook-Pro.local>
2025-11-19 04:34:15 -0800

    safe

--- client/src/pages/Deposit.jsx
+++ client/src/pages/Deposit.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
+import axios from "axios";
 import toast from "react-hot-toast";
 
 import { useState, useEffect } from "react";
